f07414877b7c3d83e6bf19223ecd1d4f5af03508
Josana-Kla <josana.0205@gmail.com>
2023-01-06 19:47:45 -0300

    database: insert sql script  to create tables

---
+++ tables.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE laboleria;
+
+CREATE TABLE cakes(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC NOT NULL,
+    image VARCHAR(2000) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE clients(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    address VARCHAR(1000) NOT NULL,
+    phone VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    "clientId" INTEGER NOT NULL REFERENCES clients("id"),
+    "cakeId" INTEGER NOT NULL REFERENCES cakes("id"),
+    quantity INTEGER NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "totalPrice" NUMERIC NOT NULL DEFAULT 0,
+    "isDelivered" BOOLEAN NOT NULL DEFAULT FALSE
+);
