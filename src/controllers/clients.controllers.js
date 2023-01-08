ebda06755102602a7230b88f8a3205aa5f54a2f0
Josana-Kla <josana.0205@gmail.com>
2023-01-08 15:59:27 -0300

    feat: added get client by id in clients controllers

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -29,6 +29,29 @@ async function createClients(req, res) {
     }
 };
 
+async function getClientOrdersById(req, res) { 
+    const { id } = req.params;
 
+    try {
+        const { rows: clientById } = await connection.query(`
+            SELECT 
+            orders.id AS "orderId", orders.quantity, orders."createdAt", orders."totalPrice",
+            cakes.name AS "cakeName" 
+            FROM orders 
+            JOIN clients ON orders."clientId" = clients.id
+            JOIN cakes ON orders."cakeId" = cakes.id
+            WHERE clients.id = $1;
+        `, [id]);
 
-export { createClients };
+        if(clientById[0]) {
+            return res.status(200).send(clientById[0]);
+        } else {
+            return res.sendStatus(404);
+        };
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+};
+
+export { createClients, getClientOrdersById };
